c75be62f01ba74ebdb17f6eeec9748c9f2983f25
Douglas DG HUB <douglascampos.b@gmail.com>
2026-04-22 22:39:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+let currentDay = "today";
+
+function setDay(day) {
+  currentDay = day;
+  document.getElementById("title").textContent =
+    day === "today" ? "Hoje" : "Amanhã";
+  loadTasks();
+}
+
+function addTask() {
+  const text = document.getElementById("taskInput").value;
+  if (text === "") return;
+
+  const task = {
+    text: text,
+    day: currentDay,
+    done: false
+  };
+
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks.push(task);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  document.getElementById("taskInput").value = "";
+  loadTasks();
+}
+
+function loadTasks() {
+  const list = document.getElementById("taskList");
+  list.innerHTML = "";
+
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+  tasks.forEach((task, index) => {
+    if (task.day !== currentDay) return;
+
+    const li = document.createElement("li");
+
+    const left = document.createElement("div");
+    left.className = "left";
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = task.done;
+
+    const span = document.createElement("span");
+    span.textContent = task.text;
+
+    if (task.done) span.style.textDecoration = "line-through";
+
+    checkbox.onclick = () => toggleTask(index);
+
+    left.appendChild(checkbox);
+    left.appendChild(span);
+
+    const del = document.createElement("button");
+    del.textContent = "X";
+    del.className = "delete";
+    del.onclick = () => deleteTask(index);
+
+    li.appendChild(left);
+    li.appendChild(del);
+
+    list.appendChild(li);
+  });
+}
+
+function toggleTask(index) {
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks[index].done = !tasks[index].done;
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  loadTasks();
+}
+
+function deleteTask(index) {
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks.splice(index, 1);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  loadTasks();
+}
+
+window.onload = loadTasks;
